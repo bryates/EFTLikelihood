d8e3fb6c4956ee7b2f7c44ecee2dbf5cab4642ce
Brent <brent.yates@email.ucr.edu>
2022-10-13 19:30:20 +0200

    Fixed Poisson eval

--- EFTLikelihood.py
+++ EFTLikelihood.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 '''
 EFT likelihood fit
 Brent R. Yates
@@ -11,7 +10,7 @@ import numpy as np
 
 class Constant():
     def __init__(self, val):
-        if isinstance(val, Constant):
+        if type(val) == Constant:
             self.val_ = val.value()
         else:
             self.val_ = val
@@ -37,6 +36,8 @@ class Constant():
         return Diff(self, rhs).simplify()
 
     def __mul__(self, rhs):
+        if type(self) == Constant and type(rhs) == Constant:
+            return Constant(self.value() * rhs.value())
         return Prod(self, rhs).simplify()
 
     def __div__(self, rhs):
@@ -49,12 +50,21 @@ class Constant():
         return Quotient(self, rhs)
 
     def __eq__(self, rhs):
-        if issubclass(type(self), Constant) and issubclass(type(rhs), int):
+        if isinstance(type(self), Constant) and issubclass(type(rhs), int):
             return self.value() == rhs
         elif not isinstance(type(rhs), type(self)):
             return False
         else:
             return self.value() == rhs.value()
+
+    def ln(self):
+        return Constant(np.log(self.value()))
+
+    def derivative(self):
+        if issubclass(type(self), Constant) or issubclass(type(self.val_), Constant):
+            return Constant(0)
+        else:
+            return self.val_.derivative()
 
     def eval(self, x=None):
         return self
@@ -76,7 +86,7 @@ class Factorial(Parameter):
         return self.param_ + '!'
 
     def eval(self, k):
-        return np.math.factorial(k)
+        return Constant(np.math.factorial(k))
 
 
 class Sum(Constant):
@@ -103,6 +113,9 @@ class Sum(Constant):
             return self.lhs_
         else:
             return Sum(self.lhs_.simplify(), self.rhs_.simplify())
+
+    def derivative(self):
+        return self.lhs_.derivative() + self.rhs_.derivative()
 
     def eval(self, x=None):
         return Constant(self.lhs_.eval(x) + self.rhs_.eval(x))
@@ -139,7 +152,14 @@ class Prod(Constant):
         self.rhs_ = rhs
 
     def value(self):
-        return self.lhs_.value() * self.rhs_.value()
+        if issubclass(type(self.lhs_.value()), int) and issubclass(type(self.rhs_.value()), int):
+            return Constant(self.lhs_.value() * self.rhs_.value())
+        elif issubclass(type(self.lhs_.value()), int):
+            return self.lhs_ * self.rhs_.value()
+        elif issubclass(type(self.rhs_.value()), int):
+            return self.lhs_.value() * self.lhs_
+        else:
+            return self.lhs_.value() * self.rhs_.value()
 
     def __str__(self):
         return '(' + str(self.lhs_) + ' * ' + str(self.rhs_) + ')'
@@ -147,15 +167,20 @@ class Prod(Constant):
     def simplify(self):
         if self.lhs_ == self.rhs_:
             return Power(self.lhs_.symbol_, Constant(2))
-        elif self.lhs_.value() == 1:
+        elif self.lhs_ == 1:
             return self.rhs_
-        elif self.rhs_.value() == 1:
+        elif self.rhs_ == 1:
             return self.lhs_
         else:
             return Prod(self.lhs_.simplify(), self.rhs_.simplify())
 
     def eval(self, x=None):
-        return Constant(self.lhs_.eval(x) * self.rhs_.eval(x))
+        if issubclass(type(self.lhs_.value()), int):
+            return Constant(self.lhs_ * self.rhs_.eval(x))
+        elif issubclass(type(self.rhs_.eval(x)), int):
+            return Constant(self.lhs_.eval(x) * self.lhs_)
+        else:
+            return Constant(self.lhs_.eval(x) * self.rhs_.eval(x))
 
 
 class Quotient(Constant):
@@ -184,6 +209,7 @@ class Quotient(Constant):
     def eval(self, x=None):
         return Constant(self.lhs_.eval(x) / self.rhs_.eval(x))
 
+
 class Variable(Constant):
     def __init__(self, symbol='x'):
         self.symbol_ = symbol
@@ -198,13 +224,27 @@ class Variable(Constant):
         return self.symbol_
 
     def __call__(self, x):
-        return self.eval(x)
+        return Constant(self.eval(x))
 
     def value(self):
         return self
 
+    def ln(self):
+        return LogVariable(self.symbol_)
+
+    def derivative(self):
+        return Constant(1)
+
     def eval(self, x):
         return Constant(x)
+
+
+class LogVariable(Variable):
+    def __init__(self, symbol='x'):
+        super().__init__(symbol)
+
+    def __str__(self):
+        return 'ln(' + str(self.symbol_) + ')'
 
 
 class Power(Variable):
@@ -229,6 +269,12 @@ class Power(Variable):
         else:
             return self
 
+    def ln(self):
+        return self.power_ * Variable(self.symbol_).ln()
+
+    def derivative(self):
+        return self.power_ * Power(self.symbol_, self.power_-1) # FIXME be carfule with x^1
+
     def eval(self, x):
         return Constant(x**self.power_.value())
 
@@ -245,6 +291,22 @@ class Polynomial(Constant):
 
     def __str__(self):
         return str(self.val_)
+
+    def ln(self):
+        return LogPolynomial(self)
+
+
+class LogPolynomial(Polynomial):
+    def __init__(self, var):
+        self.symbol_ = var.symbol_
+        self.val_ = var.val_
+
+    def __str__(self):
+        return 'ln(' + str(self.val_) + ')'
+
+    def eval(self, x):
+        return Constant(np.log(self.val_.eval(x).value()))
+
 
 class Expo(Variable):
     def __init__(self, var):
@@ -265,28 +327,70 @@ class Poisson(Prod):
         self.rhs_ = Expo(var) / Factorial(param)
 
     def eval(self, x, k):
-        var = Constant(-1) * Variable(self.symbol_)
-        self.lhs_ = Power(self.symbol_, Constant(k))
-        self.rhs_ = Expo(var) / Constant(Factorial(k).eval(k))
-        return Constant(self.lhs_.eval(x) * self.rhs_.eval(x))
+        var = Constant(-1) * Variable(k)
+        lhs = Power(self.symbol_, Constant(k)).eval(x)
+        rhs = Expo(var).eval(x) / Factorial(k).eval(k)
+        return Constant(lhs.value() * rhs.value())
 
 
 def test_Constant():
     c = Constant(2)
     print('Testing', type(c).__name__)
     print(c)
-    print((c*c + c).eval())
+    print('2*2 + 2 =', (c*c + c).eval())
+    assert (c.ln().value() - np.log(6))<1e-18
+
+def test_Variable():
+    x = Variable('x')
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+
+def test_Power():
+    x = Power('x', 2).simplify()
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print(x)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    p = Power('x', 2).simplify()
+    print('    f(x)+f(x):', end='')
+    print('   ', p+Power('x', 1))
+    print('    derivative: ', end='')
+    print('   ' + str(x.derivative()))
+
+def test_Polynomial():
+    x = Polynomial('x', [2,1,1],  2)
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    print('    derivative: ', end='')
+    print('   ' + str(x.derivative()))
 
 def test_Poisson():
     x = Poisson('x', 'k')
     print('Testing', type(x).__name__)
-    print(x)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    #print('  ' + str(x.ln()))
+    #print('    derivative: ', end='')
+    #print('   ' + str(x.derivative()))
     assert Expo(Constant(-1) * Variable('x')).eval(1), np.exp(-1)
     assert (x.eval(1,2).value() - 1**2 * np.exp(-1) / np.math.factorial(2))<1e-18 # float has precision of 1e-18
 
 
 if __name__ == '__main__':
     test_Constant()
+    test_Variable()
+    test_Power()
+    test_Polynomial()
     test_Poisson()
     '''
     test_Variable()
