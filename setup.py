e8a2e0ff7168dc297fd8d908b8806313aba99a02
Brent <brent.yates@email.ucr.edu>
2022-10-16 01:12:39 +0200

    pytest

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='EFTLikelihood',
+    version='0.0.0',
+    description='Analytic EFT likelihiod fitting',
+    packages=setuptools.find_packages(),
+)
---
+++ tests/test_EFTLikelihood.py
@@ -0,0 +1,99 @@
+from EFTLikelihood.EFTLikelihood import *
+
+def test_Constant():
+    c = EFTLikelihood.Constant(2)
+    print('Testing', type(c).__name__)
+    print(c)
+    print('2*2 + 2 =', (c*c + c).eval())
+    print((c*c + c).ln())
+    assert ((c*c + c).ln().eval() - np.log(6))<1e-18
+    assert type(c+c)==Constant
+    assert type(c-c)==Constant
+    assert type(c*c)==Constant
+    assert type(c/c)==Constant
+
+def test_Variable():
+    x = Variable('x')
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    print('    derivative: ', end='')
+    print('  ' + str(x.derivative()))
+    print('    f\'(ln(x)): ', end='')
+    print('  ' + str(x.ln().derivative()))
+    assert ((Constant(2)*x-x).ln().eval(2) - np.log(2*2 - 2))<1e-18
+    assert ((Constant(2)*x-x).ln().eval(1) - np.log(2*1 - 1))<1e-18
+    assert ((Constant(2)*x-x).ln().eval(2) - np.log(2*2 - 2))<1e-18
+    assert type(x.eval(1)+x.eval(1))==Constant
+    assert type(x.eval(1)-x.eval(1))==Constant
+    assert type(x.eval(1)*x.eval(1))==Constant
+    assert type(x.eval(1)/x.eval(1))==Constant
+
+def test_Power():
+    x = Power('x', 2).simplify()
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print(x)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    p = Power('x', 2).simplify()
+    print('    f(x)+f(x):', end='')
+    print('   ', p+Power('x', 1))
+    print('    derivative: ', end='')
+    print('   ' + str(x.derivative()))
+    assert (x.eval(2) - 2**2)<1e-18
+    assert type(x.eval(1)+x.eval(1))==Constant
+    assert type(x.eval(1)-x.eval(1))==Constant
+    assert type(x.eval(1)*x.eval(1))==Constant
+    assert type(x.eval(1)/x.eval(1))==Constant
+
+def test_Polynomial():
+    x = Polynomial('x', [2,1,1],  2)
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    print('    derivative: ', end='')
+    print('   ' + str(x.derivative()))
+    assert (x.eval(2) - (2**2 + 2**1 + 1))<1e-18
+    assert type(x.eval(1)+x.eval(1))==Constant
+    assert type(x.eval(1)-x.eval(1))==Constant
+    assert type(x.eval(1)*x.eval(1))==Constant
+    assert type(x.eval(1)/x.eval(1))==Constant
+
+def test_Expo():
+    x = Expo(Prod(Constant(-1), Variable('x')))
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    print('    derivative: ', end='')
+    print('   ' + str(x.derivative()))
+    assert (x.eval(2) - np.exp(-2))<1e-18
+    assert (x.derivative().eval(2) - -np.exp(-2))<1e-18
+    assert type(x.eval(1)+x.eval(1))==Constant
+    assert type(x.eval(1)-x.eval(1))==Constant
+    assert type(x.eval(1)*x.eval(1))==Constant
+    assert type(x.eval(1)/x.eval(1))==Constant
+
+def test_Poisson():
+    x = Poisson('x', 'k')
+    print('Testing', type(x).__name__)
+    print('    function: ', end='')
+    print('   ' + str(x))
+    print('    ln: ', end='')
+    print('  ' + str(x.ln()))
+    print('    derivative: ', end='')
+    print('   ' + str(x.derivative()))
+    print('    f\'(ln(x)): ', end='')
+    print('  ' + str(x.ln().derivative()))
+    assert (x.eval(1,2) - (1**2 * np.exp(-1) / np.math.factorial(2)))<1e-18 # float has precision of 1e-18
+    assert (x.derivative().eval(1,2) - np.exp(-1)/2)<1e-18 # float has precision of 1e-18
+    assert (x.derivative().eval(1,10) - (np.exp(-1)*(10-1)*(1**(10-1)/np.math.factorial(10))))<1e-18 # float has precision of 1e-18
+    assert (x.ln().derivative().eval(2,10) - (10/2-1))<1e-18 # float has precision of 1e-18
